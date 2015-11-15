4227733b64e947b297bfd0e8e2764962e4281a53
Junk Chen <junkchen@aliyun.com>
2015-11-16 07:20:43 +0800

    Initial commit

---
+++ app/src/main/java/com/junkchen/library/MainActivity.java
@@ -0,0 +1,25 @@
+package com.junkchen.library;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
